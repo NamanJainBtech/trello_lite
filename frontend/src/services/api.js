3dcf3bdd460b8ef4f7ac1cb90e99c034c94e864b
NamanJainBtech <namanjain2022@jklu.edu.in>
2025-11-26 16:44:47 +0530

    render rem

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://trello-lite-qxwu.onrender.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api',
 });
 
 API.interceptors.request.use((config) => {
